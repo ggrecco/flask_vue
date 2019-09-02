a1c4bf9a59216437724a04e4394b18c8d3795d2f
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-09-01 22:45:43 -0300

    renderizando um json dicionaro com vueJS + flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,7 +10,8 @@ def index():
 
 @app.route('/teste')
 def teste():
-    return 'teste funcionou'
+    meu_dicionario = {"title": "Nightwish", "genre": "MetalSynphonico"}
+    return jsonify(meu_dicionario)
 
 
 @app.route('/sobre')
